c85fed4e0017cde33af8f2f7bedf3eba7d161cac
Rheza Pahlevi <damniatx@gmail.com>
2012-03-24 22:56:19 +0800

    Add dummy menu items and dummy actions.

--- src/com/redditandroiddevelopers/googletasksclient/GoogleTasksClientActivity.java
+++ src/com/redditandroiddevelopers/googletasksclient/GoogleTasksClientActivity.java
@@ -2,33 +2,75 @@ package com.redditandroiddevelopers.googletasksclient;
 
 
 
-import android.app.Activity;
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
-
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuInflater;
 
 
 public class GoogleTasksClientActivity extends SherlockActivity {
 	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+       //add menu items to the action bar. look on onOptionsItemSelected to action code.
+		
+        menu.add(0,0,0,"Add New Task"); 
+        menu.add(0,1,0,"Search"); 
+        menu.add(0,2,0,"Settings");    
+
+       
+
+         
+        return true;
+    }
 	
-	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+		 
+		switch(item.getItemId())
+        {
+            case 0:
+            {
+          	Toast.makeText(this, "Add selected",
+    	              Toast.LENGTH_SHORT).show();
+           	return true;
+            }
+            case 1:
+            {
+           Toast.makeText(this, "Search selected",
+    	             Toast.LENGTH_SHORT).show();
+           	return true;
+            }
+            case 2:
+            {
+           Toast.makeText(this, "Settings selected",
+    	             Toast.LENGTH_SHORT).show();
+           	return true;
+            }
+            default:
+        		return super.onOptionsItemSelected(item);
+        }
+    	
+    }
 	 
     /** Called when the activity is first created. */
 	 @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       
-    
-      
- 		
+
         getSupportActionBar().setTitle("Reddit Google Tasks");
 
-       setContentView(R.layout.main);
+        setContentView(R.layout.main);
         
     }
     
-    
+	 
    
 }
