b58480fbff456f2196f43e217ba3206e950b69e3
Rheza Pahlevi <damniatx@gmail.com>
2012-03-24 22:14:15 +0800

    Maven and ActionBarSherlock Integration Done
    we need to manually add actionbarsherlock as a library in eclipse, as
    stated by ActionBarSherlock Dev. http://goo.gl/jaSQd . Check this out.
    http://i.imgur.com/ZQR9f.png

--- src/com/redditandroiddevelopers/googletasksclient/GoogleTasksClientActivity.java
+++ src/com/redditandroiddevelopers/googletasksclient/GoogleTasksClientActivity.java
@@ -5,10 +5,12 @@ package com.redditandroiddevelopers.googletasksclient;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
 
 
 
-public class GoogleTasksClientActivity extends Activity {
+public class GoogleTasksClientActivity extends SherlockActivity {
 	
 	
 	
@@ -17,13 +19,13 @@ public class GoogleTasksClientActivity extends Activity {
 	 @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       /*
-        setTheme(com.actionbarsherlock.R.style.Theme_Sherlock);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       
+    
+      
  		
         getSupportActionBar().setTitle("Reddit Google Tasks");
-*/
-        setContentView(R.layout.main);
+
+       setContentView(R.layout.main);
         
     }
     
